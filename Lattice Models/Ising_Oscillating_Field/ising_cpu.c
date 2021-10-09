cec2f3d1e51194b44e4ec212553e264051769866
William Carreras <37045478+williamGOC@users.noreply.github.com>
2021-10-08 21:15:20 -0300

    Update ising_cpu.c

--- Lattice Models/Ising_Oscillating_Field/ising_cpu.c
+++ Lattice Models/Ising_Oscillating_Field/ising_cpu.c
@@ -1,80 +1,24 @@
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
-#define UP 1
-#define DOWN -1
-
-#define DIM 2
-#define L 500
-
-#define N_SWEEPS 10000
-
-#define IM1 2147483563
-#define IM2 2147483399
-#define AM (1.0/IM1)
-#define IMM1 (IM1-1)
-#define IA1 40014
-#define IA2 40692
-#define IQ1 53668
-#define IQ2 52774
-#define IR1 12211
-#define IR2 3791
-#define NTAB 32
-#define NDIV (1+IMM1/NTAB)
-#define EPS 1.2e-7
-#define RNMX (1.0-EPS)
+#include "ising_cpu.h"
 
 
-typedef struct system{
-	
-	int *spins;
-	unsigned *neighbors;
+IsingModel makeSystem(double _J, double _T, double _H){
 
-	int N;
-	long seed;
-	int step;
+    // Memory allocation for the central structure of the program.
+	IsingModel pS = (IsingModel)malloc(sizeof(struct system));		
+	assert(pS);
 
-	double J, T, H;
-
-} *GlauberModel;
-
-
-// System Structuring Functions:
-GlauberModel makeSystem(double _J, double _omeg, double _H);
-void destroySystem(GlauberModel pG);
-void printerSystem(GlauberModel pG, FILE * gnuplotPIPE);
-
-
-// Dynamical Evolution Functions:
-void stepMC(GlauberModel pG);
-float rand2(long *idum);
-unsigned getId(int x, int y);
-double getm(GlauberModel pG);
-
-
-
-GlauberModel makeSystem(double _J, double _T, double _H){
-
-
-	// Memory allocation for the central structure of the program.
-	GlauberModel pG = (GlauberModel)malloc(sizeof(struct system));		
-	assert(pG);
-
-	pG -> N = (int)pow(L, DIM);
+	pS -> N = (int)pow(L, DIM);
 
 	// Creation of dynamic array member of the central structure.
-	pG -> spins = (int *)malloc(pG -> N * sizeof(int));
-	assert(pG -> spins);
+	pS -> spins = (int *)malloc(pS -> N * sizeof(int));
+	assert(pS -> spins);
 
-	pG -> seed = -time(NULL);
-	pG -> step = 0;
+	pS -> seed = -time(NULL);
+	pS -> step = 0;
 
-	pG -> J = _J;
-	pG -> T = _T;
-	pG -> H = _H;
+	pS -> J = _J;
+	pS -> T = _T;
+	pS -> H = _H;
 
 	
 	// Calculate lattice volume elements:
@@ -88,16 +32,16 @@ GlauberModel makeSystem(double _J, double _T, double _H){
 
 
 	// Neighborhood table:
-	pG -> neighbors = (unsigned *)malloc(2 * DIM * pG -> N *sizeof(unsigned));
-	assert(pG -> neighbors);
+	pS -> neighbors = (unsigned *)malloc(2 * DIM * pS -> N *sizeof(unsigned));
+	assert(pS -> neighbors);
 
 	
 	
-	for (i = 0; i < pG -> N; ++i){
+	for (i = 0; i < pS -> N; ++i){
 		unsigned short k = 0;
 
 		unsigned short dim; for (dim = 0; dim < DIM; ++dim){					// dimension loop
-			short dir; for (dir = -1; dir <= 1; dir += 2){ 			// two directions in each dimension
+			short dir; for (dir = -1; dir <= 1; dir += 2){ 			            // two directions in each dimension
 
 				// neighbor's id in spin list:
 				int npos = i + dir * volume[dim];
@@ -110,61 +54,59 @@ GlauberModel makeSystem(double _J, double _T, double _H){
 				else if (test >= volume[dim+1])
 					npos -= volume[dim+1];
 
-				pG -> neighbors[2*DIM*i + k] = npos;
+				pS -> neighbors[2*DIM*i + k] = npos;
 				k++;
 			}
 		}
 
-		
-			
-		pG -> spins[i] =  (int)(2 * floor(2 * rand2(&pG-> seed)) - 1);   // create a random system
-		//pG -> spins[i] =  UP;   // create a UP-system	
+		pS -> spins[i] =  (int)(2 * floor(2 * rand2(&pS-> seed)) - 1);   // create a random system
+		//pS -> spins[i] =  UP;   // create a UP-system	
 	}
 
-	return pG;
+	return pS;
 }
 
 
 
-void stepMC(GlauberModel pG){
+void stepMC(IsingModel pS){
 
 
-	unsigned i, j; for (i = 0; i < pG -> N; ++i){
+	unsigned i, j; for (i = 0; i < pS -> N; ++i){
 		
-		unsigned index = (unsigned)(pG -> N * rand2(&pG -> seed));
-		int spinstate = pG -> spins[index];
+		unsigned index = (unsigned)(pS -> N * rand2(&pS -> seed));
+		int spinstate = pS -> spins[index];
 
 		double deltaE = 0.0;
 
 		for (j = 0; j < 2 * DIM; ++j)
-			deltaE += pG -> J * pG -> spins[pG -> neighbors[2*DIM*index + j]];
+			deltaE += pS -> J * pS -> spins[pS -> neighbors[2*DIM*index + j]];
 
-		deltaE = (pG -> step > 0.2*N_SWEEPS)?(2.0 * spinstate * (deltaE + pG -> H * cos((2 * M_PI * (pG -> step - 0.2*N_SWEEPS)) / pG -> T))):(2.0 * spinstate * deltaE);
+		deltaE = (pS -> step > 0.2*N_SWEEPS)?(2.0 * spinstate * (deltaE + pS -> H * cos((2 * M_PI * (pS -> step - 0.2*N_SWEEPS)) / pS -> T))):(2.0 * spinstate * deltaE);
 		
 
-		if (exp(-1.0 * deltaE) >= rand2(&pG -> seed))
-			pG -> spins[index] = - spinstate;
+		if (exp(-1.0 * deltaE) >= rand2(&pS -> seed))
+			pS -> spins[index] = - spinstate;
 
 	}
 
-	++pG -> step;
+	++pS -> step;
 }
 
-double getm(GlauberModel pG){
+double getm(IsingModel pS){
 
 	int M = 0;
-	int i; for (i = 0; i < pG -> N; ++i)
-		M += pG -> spins[i];
+	int i; for (i = 0; i < pS -> N; ++i)
+		M += pS -> spins[i];
 
-	return (1.0 * M) / (1.0 * pG -> N);
+	return (1.0 * M) / (1.0 * pS -> N);
 }
 
 
-void destroySystem(GlauberModel pG){
+void destroySystem(IsingModel pS){
 
-	free(pG -> spins);
-	free(pG -> neighbors);
-	free(pG);
+	free(pS -> spins);
+	free(pS -> neighbors);
+	free(pS);
 }
 
 
@@ -174,55 +116,66 @@ unsigned getId(int x, int y){
 
 
 
-void printerSystem(GlauberModel pG, FILE * gnuplotPIPE){
+void printerSystem(IsingModel pS, FILE * gnuplotPIPE){
 
-	fprintf(gnuplotPIPE, "set title '{/=20 Glauber Model}'\n");
+	fprintf(gnuplotPIPE, "set title '{/=20 Ising Model}'\n");
 	fprintf(gnuplotPIPE, "unset xtics; unset ytics\n");
 	fprintf(gnuplotPIPE, "unset border\n");
 	fprintf(gnuplotPIPE, "set xrange [0:%d]; set yrange [0:%d]\n", L, L);
 	fprintf(gnuplotPIPE, "plot '-' u 1:2:3 w p pt 5 ps 3 lc variable title ''\n");
 
-	int i; for(i = 0; i < pG -> N; i++)
-		fprintf(gnuplotPIPE, "%d\t%d\t%d\n", i%L, i/L, pG -> spins[i]);
+	int i; for(i = 0; i < pS -> N; i++)
+		fprintf(gnuplotPIPE, "%d\t%d\t%d\n", i%L, i/L, pS -> spins[i]);
 
 	fprintf(gnuplotPIPE, "e\n");
 	fflush(gnuplotPIPE);
 }
 
+void addSquare(int x, int y, int w, int sign, IsingModel pS){
+		
+	int i, j;
+	int ww=(w>L)?(L):(w);
+		
+	for(j=y-ww/2;j<y+ww/2;j++){
+		for(i=x-ww/2;i<x+ww/2;i++){
+			pS -> spins[(i+L)%L+((j+L)%L)*L]=sign;
+		}
+	}
+}
 
 
 float rand2(long *idum){
     
-  int j;
-  long k;
+    int j;
+    long k;
   
-  static long idum2=123456789;
-  static long iy=0;
-  static long iv[NTAB];
-  float temp;
+    static long idum2=123456789;
+    static long iy=0;
+    static long iv[NTAB];
+    float temp;
 
-  if (*idum <= 0){                        //Initialize.
-  if (-(*idum) < 1) *idum=1;              //Be sure to prevent idum = 0.
-  else *idum = -(*idum);
-  idum2=(*idum);
-  for (j=NTAB+7;j>=0;j--){                //Load the shuffle table (after 8 warm-ups).
-  k=(*idum)/IQ1;
-  *idum=IA1*(*idum-k*IQ1)-k*IR1;
-  if (*idum < 0) *idum += IM1;
-  if (j < NTAB) iv[j] = *idum;
-  }
-  iy=iv[0];
-  }
-  k=(*idum)/IQ1;                           //Start here when not initializing.
-  *idum=IA1*(*idum-k*IQ1)-k*IR1;           //Compute idum=(IA1*idum) % IM1 without
-  if (*idum < 0) *idum += IM1;             //overflows by Schrage’s method.
-  k=idum2/IQ2;
-  idum2=IA2*(idum2-k*IQ2)-k*IR2;           //Compute idum2=(IA2*idum) % IM2 likewise.
-  if (idum2 < 0) idum2 += IM2;
-  j=iy/NDIV;                               //Will be in the range 0..NTAB-1.
-  iy=iv[j]-idum2;                          //Here idum is shuffled, idum and idum2 are
-  iv[j] = *idum;                           //combined to generate output.
-  if (iy < 1) iy += IMM1;
-  if ((temp=AM*iy) > RNMX) return RNMX;    //Because users don’t expect endpoint values.
-  else return temp;
+    if (*idum <= 0){                        //Initialize.
+    if (-(*idum) < 1) *idum=1;              //Be sure to prevent idum = 0.
+    else *idum = -(*idum);
+    idum2=(*idum);
+    for (j=NTAB+7;j>=0;j--){                //Load the shuffle table (after 8 warm-ups).
+    k=(*idum)/IQ1;
+    *idum=IA1*(*idum-k*IQ1)-k*IR1;
+    if (*idum < 0) *idum += IM1;
+    if (j < NTAB) iv[j] = *idum;
+    }
+    iy=iv[0];
+    }
+    k=(*idum)/IQ1;                           //Start here when not initializing.
+    *idum=IA1*(*idum-k*IQ1)-k*IR1;           //Compute idum=(IA1*idum) % IM1 without
+    if (*idum < 0) *idum += IM1;             //overflows by Schrage’s method.
+    k=idum2/IQ2;
+    idum2=IA2*(idum2-k*IQ2)-k*IR2;           //Compute idum2=(IA2*idum) % IM2 likewise.
+    if (idum2 < 0) idum2 += IM2;
+    j=iy/NDIV;                               //Will be in the range 0..NTAB-1.
+    iy=iv[j]-idum2;                          //Here idum is shuffled, idum and idum2 are
+    iv[j] = *idum;                           //combined to generate output.
+    if (iy < 1) iy += IMM1;
+    if ((temp=AM*iy) > RNMX) return RNMX;    //Because users don’t expect endpoint values.
+    else return temp;
 }
